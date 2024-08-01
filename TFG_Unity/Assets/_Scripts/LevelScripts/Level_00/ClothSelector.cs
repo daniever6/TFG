efe8ece0e7d86ec7c333cc8b0c8763fe1fcbca1b
zdpgonzalo <zdpgonzalo@gmail.com>
2024-08-01 22:52:09 +0200

    UI seleccion de ropa terminada
    Se ha hecho la interfaz que sea responsive, ahora los elementos seleccionados estan marcados con un outline y los no seleccionados se ven con un colortint gris

--- TFG_Unity/Assets/_Scripts/LevelScripts/Level_00/ClothSelector.cs
+++ TFG_Unity/Assets/_Scripts/LevelScripts/Level_00/ClothSelector.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Scripts.LevelScripts.Level_00
 {
@@ -12,13 +13,26 @@ namespace _Scripts.LevelScripts.Level_00
         Pants,
         Shoes
     }
+    
+    [Serializable]
+    public class ClothItemContainer
+    {
+        public BodyPart key;
+        public List<Image> collection;
+    }
+    
     public class ClothSelector : MonoBehaviour
     {
+        [SerializeField] private List<ClothItemContainer> clothButtons;
+        
         [SerializeField] private List<GameObject> hairComponents;
         [SerializeField] private List<GameObject> shirtComponents;
         [SerializeField] private List<GameObject> gloveComponents;
         [SerializeField] private List<GameObject> pantsComponents;
         [SerializeField] private List<GameObject> shoesComponents;
+        
+        private Color desactivateColor = new Color(0.6f, 0.6f,0.6f, 1);
+        private Color outlineColor = Color.black;
 
         private int currentHairIdx = 2;
         private int currentShirtIdx = -1;
@@ -33,10 +47,61 @@ namespace _Scripts.LevelScripts.Level_00
             ActivateComponent(gloveComponents, currentGloveIdx);
             ActivateComponent(pantsComponents, currentPantsIdx);
             ActivateComponent(shoesComponents, currentShoesIdx);
+
+            foreach (var itemContainer in clothButtons)
+            {
+                switch (itemContainer.key)
+                {
+                    case BodyPart.Hair:
+                        SetItemColor(itemContainer.collection, currentHairIdx + 1 );
+                        break;
+                    
+                    case BodyPart.Shirt:
+                        SetItemColor(itemContainer.collection, currentShirtIdx + 1 );
+                        break;
+                    
+                    case BodyPart.Glove:
+                        SetItemColor(itemContainer.collection, currentGloveIdx + 1 );
+                        break;
+                    
+                    case BodyPart.Pants:
+                        SetItemColor(itemContainer.collection, currentPantsIdx + 1);
+                        break;
+                    
+                    case BodyPart.Shoes:
+                        SetItemColor(itemContainer.collection, currentShoesIdx);
+                        break;
+                }
+            }
         }
 
         /// <summary>
-        /// Activa los componentes seleccionados y los otros los desactiva
+        /// Establece el color por defecto de los botones de la interfaz. Si no esta seleccionado, sera gris,
+        /// si esta seleccionado sera blanco
+        /// </summary>
+        /// <param name="collection">Lista de botones de ropa</param>
+        /// <param name="currentIdx">Boton activado</param>
+        private void SetItemColor(List<Image> collection, int currentIdx)
+        {
+            int cont = 0;
+            foreach (var button in collection)
+            {
+                Outline outline = button.gameObject.AddComponent<Outline>();
+                outline.effectColor = outlineColor;
+                outline.effectDistance = new Vector2(4, 4);
+
+                if (cont != currentIdx)
+                {
+                    outline.enabled = false;
+                }
+                
+                button.color = (cont == currentIdx) ? Color.white : desactivateColor;
+                cont++;
+            }
+        }
+
+        /// <summary>
+        /// Activa los componentes(Ropa) seleccionados y los otros los desactiva
         /// </summary>
         /// <param name="components">Lista de componentes</param>
         /// <param name="activeIdx">Indice seleccionado</param>
@@ -60,18 +125,23 @@ namespace _Scripts.LevelScripts.Level_00
             switch (bodyPart)
             {
                 case BodyPart.Hair:
+                    ItemSelectionColor(currentHairIdx + 1, idxSelected + 1, BodyPart.Hair);
                     SelectionLogic(ref currentHairIdx, idxSelected, hairComponents);
                     break;
                 case BodyPart.Shirt:
+                    ItemSelectionColor(currentShirtIdx + 1, idxSelected + 1, BodyPart.Shirt);
                     SelectionLogic(ref currentShirtIdx, idxSelected, shirtComponents);
                     break;
                 case BodyPart.Glove:
+                    ItemSelectionColor(currentGloveIdx + 1, idxSelected + 1, BodyPart.Glove);
                     SelectionLogic(ref currentGloveIdx, idxSelected, gloveComponents);
                     break;
                 case BodyPart.Pants:
+                    ItemSelectionColor(currentPantsIdx + 1, idxSelected + 1, BodyPart.Pants);
                     SelectionLogic(ref currentPantsIdx, idxSelected, pantsComponents);
                     break;
                 case BodyPart.Shoes:
+                    ItemSelectionColor(currentShoesIdx, idxSelected, BodyPart.Shoes);
                     SelectionLogic(ref currentShoesIdx, idxSelected, shoesComponents);
                     break;
             }
@@ -95,5 +165,23 @@ namespace _Scripts.LevelScripts.Level_00
             
             items[newIdx].SetActive(true);
         }
+
+        /// <summary>
+        /// Cambia el color del boton del item seleccionado
+        /// </summary>
+        /// <param name="currentIdx">Item anterior</param>
+        /// <param name="newIdx">Item seleccionado</param>
+        /// <param name="bodyPart">Parte del cuerpo</param>
+        public void ItemSelectionColor(int currentIdx ,int newIdx, BodyPart bodyPart)
+        {
+            var buttons = clothButtons[(int)bodyPart].collection;
+
+            buttons[currentIdx].color = desactivateColor;
+            buttons[currentIdx].gameObject.GetComponent<Outline>().enabled = false;
+            
+            buttons[newIdx].color = Color.white;
+            buttons[newIdx].gameObject.GetComponent<Outline>().enabled = true;
+        }
+
     }
 }
