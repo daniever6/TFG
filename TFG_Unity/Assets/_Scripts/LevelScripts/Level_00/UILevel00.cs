cd5fe73c97c5015647061f2ed1d29ec17f613dc9
zdpgonzalo <zdpgonzalo@gmail.com>
2024-08-06 21:14:55 +0200

    Interfaz de seleccion de ropa
    Se han añadido nuevas imagenes y se ha implementado parte de la logica del nivel del cambiado de ropa

--- TFG_Unity/Assets/_Scripts/LevelScripts/Level_00/UILevel00.cs
+++ TFG_Unity/Assets/_Scripts/LevelScripts/Level_00/UILevel00.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using _Scripts.Utilities;
 using Cinemachine;
@@ -9,13 +10,25 @@ namespace _Scripts.LevelScripts.Level_00
     public class UILevel00 : GameplayMonoBehaviour<UILevel00>
     {
         [SerializeField] private Canvas clothingCanvas;
+        [SerializeField] private Canvas endEditCanvas;
+        private Canvas currentActiveCanvas;
+        
         [SerializeField] private CinemachineVirtualCamera generalCamera;
         [SerializeField] private List<GameObject> bodyCameras;
         [SerializeField] private List<GameObject> bodyUIPanels;
         [SerializeField] private TextMeshProUGUI headerTitle;
+        
         private string[] _bodyParts = { "Peinado", "Torso", "Manos", "Pantalones", "Calzado" };
         private int _currentBodyPart = 0;
         private bool _isCanvasOpen = true;
+
+        private void Start()
+        {
+            _isCanvasOpen = true;
+            currentActiveCanvas = clothingCanvas;
+            currentActiveCanvas.gameObject.SetActive(true);
+            endEditCanvas.gameObject.SetActive(false);
+        }
 
         /// <summary>
         /// Cambia la parte del cuerpo en la UI del selector de ropa
@@ -42,9 +55,7 @@ namespace _Scripts.LevelScripts.Level_00
         /// </summary>
         public void OpenClothSelector()
         {
-            _isCanvasOpen = true;
-            
-            clothingCanvas.enabled = true;
+            ChangeCurrentActiveCanvas(clothingCanvas);
             _currentBodyPart = 0;
             
             bodyUIPanels[_currentBodyPart].SetActive(true);
@@ -57,9 +68,7 @@ namespace _Scripts.LevelScripts.Level_00
         /// </summary>
         public void AcceptChanges()
         {
-            _isCanvasOpen = false;
-            
-            clothingCanvas.enabled = false;
+            ChangeCurrentActiveCanvas(endEditCanvas);
 
             bodyUIPanels[_currentBodyPart].SetActive(false);
             bodyCameras[_currentBodyPart].SetActive(false);
@@ -67,10 +76,24 @@ namespace _Scripts.LevelScripts.Level_00
             generalCamera.enabled = true;
         }
 
+        /// <summary>
+        /// Activa el nuevo canvas del nivel y desactiva el canvas que estaba activo
+        /// </summary>
+        /// <param name="canvas">Nuevo canvas a activar</param>
+        private void ChangeCurrentActiveCanvas(Canvas canvas)
+        {
+            currentActiveCanvas.gameObject.SetActive(false);
+
+            currentActiveCanvas = canvas;
+
+            currentActiveCanvas.gameObject.SetActive(true);
+        }
+
         protected override void OnPostPaused()
         {
             base.OnPostPaused();
-            clothingCanvas.enabled = false;
+
+            currentActiveCanvas.enabled = false;
         }
 
         protected override void OnPostResumed()
@@ -78,7 +101,7 @@ namespace _Scripts.LevelScripts.Level_00
             base.OnPostResumed();
             if (_isCanvasOpen)
             {
-                clothingCanvas.enabled = true;
+                currentActiveCanvas.enabled = true;
             }
         }
     }
